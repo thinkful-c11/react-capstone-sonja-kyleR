5742846aec56d5312240e769e8f2683538183217
Kyle Rogers <kylerogers334@gmail.com>
2017-07-11 14:45:33 +0000

    Add schemas

---
+++ server/models/categorySchema.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const categorySchema = mongoose.Schema({
+    name: { type: String, require: true },
+    taste: { type: String, require: true }
+});
+
+const Category = mongoose.model('Category', categorySchema);
+module.exports = {Category};
