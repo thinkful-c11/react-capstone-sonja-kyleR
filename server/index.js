4f547d5de28575985c15661ec618f03a6447b853
Kyle Rogers <kylerogers334@gmail.com>
2017-07-11 15:24:22 +0000

    Add get and post endpoints

--- server/index.js
+++ server/index.js
@@ -1,3 +1,9 @@
+//schemas
+const Tastes = require('./models/tasteSchema');
+const Categories = require('./models/categorySchema');
+const UnhealthyFoods = require('./models/unhealthyFoodsSchema');
+const HealthyFoods = require('./models/healthyFoodsSchema');
+
 const path = require('path');
 const express = require('express');
 
@@ -6,8 +12,107 @@ const app = express();
 // API endpoints go here!
 
 app.get('/', (req, res) => {
-    res.json({hello: 'world'});
-})
+    Tastes
+        .find()
+        .exec()
+        .then(tastes => {
+            res.json(tastes);
+        })
+        .catch(err => console.error(err));
+});
+
+app.get('/:taste', (req, res) => {
+    Categories
+        .find({taste: req.params.taste})
+        .exec()
+        .then(category => {
+            res.json(category);
+        })
+        .catch(err => console.error(err));
+});
+
+app.get('/:taste/:category', (req, res) => {
+    UnhealthyFoods
+        .find({
+            taste: req.params.taste,
+            category: req.params.category
+        })
+        .exec()
+        .then(unhealthyfood => {
+            res.json(unhealthyfood);
+        })
+        .catch(err => console.error(err));
+});
+
+app.get('/:taste/:category/:unhealthyfood', (req, res) => {
+    HealthyFoods
+        .find({
+            correspondingUnhealthyFood: req.params.unhealthyfood,
+        })
+        .exec()
+        .then(healthyfood => {
+            res.json(healthyfood);
+        })
+        .catch(err => console.error(err));
+});
+
+app.post('/:taste', (req, res) => {
+    Tastes
+        .create({name: req.params.taste})
+        .then(taste => res.status(201).json(taste))
+        .catch(err => {
+            res.status(500).json({
+                message: 'Failed to create taste',
+                error: err
+            });
+        });
+});
+
+app.post('/:taste/:category', (req, res) => {
+    Categories
+        .create({
+            taste: req.params.taste,
+            name: req.params.category
+        })
+        .then(category => res.status(201).json(category))
+        .catch(err => {
+            res.status(500).json({
+                message: 'Failed to create category',
+                error: err
+            });
+        });
+});
+
+app.post('/:taste/:category/:unhealthyfood', (req, res) => {
+    UnhealthyFoods
+        .create({
+            taste: req.params.taste,
+            category: req.params.category,
+            name: req.params.unhealthyfood
+        })
+        .then(unhealthyfood => res.status(201).json(unhealthyfood))
+        .catch(err => {
+            res.status(500).json({
+                message: 'Failed to create unhealthyfood',
+                error: err
+            });
+        });
+});
+
+app.post('/:taste/:category/:unhealthyfood/:healthyfood', (req, res) => {
+    HealthyFoods
+        .create({
+            name: req.params.healthfood,
+            correspondingUnhealthyFood: req.params.unhealthyfood
+        })
+        .then(healthyfood => res.status(201).json(healthyfood))
+        .catch(err => {
+            res.status(500).json({
+                message: 'Failed to create unhealthyfood',
+                error: err
+            });
+        });
+});
 
 // Serve the built client
 app.use(express.static(path.resolve(__dirname, '../client/build')));
