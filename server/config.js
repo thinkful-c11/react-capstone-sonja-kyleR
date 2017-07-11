05b78ad2c6f33edfea0c4cca5e24f365d207c490
Kyle Rogers <kylerogers334@gmail.com>
2017-07-11 17:08:52 +0000

    Add config

---
+++ server/config.js
@@ -0,0 +1,9 @@
+'use strict';
+require('dotenv').config();
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL
+                       
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+                       global.TEST_DATABASE_URL
+                      
+exports.PORT = process.env.PORT || 8080;
