b2648a5a21b532fa0b97069fb3adf37e1b229323
Sonja Duric <duric.sonja@gmail.com>
2017-07-12 12:45:29 -0400

    new craving post endpoint functional

--- client/src/components/AddOptionForm.js
+++ client/src/components/AddOptionForm.js
@@ -1,14 +1,30 @@
-import React from 'react'; //, { Component } 
+import React, {Component} from 'react'; //
+import {connect} from 'react-redux';
+
+import {postUnhealthy} from '../actions';
 import './AddOptionForm.css';
 
-export default function AddOptionForm(){
+export class AddOptionForm extends Component{
 
-    return(
-        <div className='addOption'>
-            Don't see your craving? Tell us what you're after!
-            <input type="text" placeholder="Vegan cheese"></input>
-            <button>Tell us!</button>
-        </div>
-    );
+    onSubmit(event){
+        event.preventDefault();
+        const value = this.input.value;
+        this.props.dispatch(postUnhealthy({name:value}));
+    }
+
+    render(){
+        return(
+            <div className='addOption'>
+                Don't see your craving? Tell us what you're after!
+                <form onSubmit={e => this.onSubmit(e)}>
+                    <input type="text" placeholder="Vegan cheese" 
+                        ref={input => this.input = input} required/>
+                    <button type="submit">Tell us!</button>
+                </form>
+            </div>
+        );
+    }
 
 }
+
+export default connect()(AddOptionForm);
