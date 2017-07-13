0d139884b73bbe9d72ba17aaf73806060c9de7c4
Kyle Rogers <kylerogers334@gmail.com>
2017-07-13 18:12:47 +0000

    Finish tests

--- test/action.test.js
+++ test/action.test.js
@@ -5,28 +5,28 @@ import * as actions from '../client/src/actions/index';
 describe('selectCraving()', function() {
     it('should return the action', function() {
         const action = actions.selectCraving('chocolate');
-        expect(action.type).to.equal(actions.SELECT_CRAVING);
-        expect(action.craving).to.equal('chocolate');
+        expect(action.type).toEqual(actions.SELECT_CRAVING);
+        expect(action.craving).toEqual('chocolate');
     });
 });
 
 describe('addOtherCraving()', function() {
     it('should return the action', function() {
         const action = actions.addOtherCraving();
-        expect(action.type).to.equal(actions.ADD_OTHER_CRAVING);
+        expect(action.type).toEqual(actions.ADD_OTHER_CRAVING);
     });
 });
 
 describe('resetDefaults()', function() {
     it('should return the action', function() {
         const action = actions.resetDefaults();
-        expect(action.type).to.equal(actions.RESET_DEFAULTS);
+        expect(action.type).toEqual(actions.RESET_DEFAULTS);
     });
 });
 // End sync actions
 
 // Async actions
-describe.only('fetchUnhealthy()', function() {
+describe('fetchUnhealthy()', function() {
     it('should dispatch fetchUnhealthyRequest', function() {
         global.fetch = jest.fn().mockImplementation( url => {
             return new Promise( (res, rej) => res({ok:true, json() { return {}; }}));
@@ -70,3 +70,49 @@ describe.only('fetchUnhealthy()', function() {
         });
     });
 });
+
+describe('fetchHealthy', function() {
+    it('should dispatch fetchHealthyRequest', function() {
+        global.fetch = jest.fn().mockImplementation( url => {
+            return new Promise( (res, rej) => res({ok:true, json() { return {}; }}));
+        });
+        const dispatch = jest.fn();
+
+        return actions.fetchHealthyStuff()(dispatch).then(() => {
+            // Check that we made a request to the correct URL
+            expect(fetch).toHaveBeenCalled();
+            // Make sure that we dispatched the correct sync action
+            expect(dispatch).toHaveBeenCalledWith(actions.fetchHealthyRequest());
+        });
+    });
+    
+    it('should dispatch fetchHealthySuccess', function() {
+        global.fetch = jest.fn().mockImplementation( url => {
+            return new Promise( (res, rej) => res({ok:true, json() { return {}; }}));
+        });
+        const dispatch = jest.fn();
+
+        return actions.fetchHealthyStuff('chocolate')(dispatch).then(() => {
+            // Check that we made a request to the correct URL
+            expect(fetch).toHaveBeenCalledWith('/api/healthy/chocolate');
+            // Make sure that we dispatched the correct sync action
+            expect(dispatch).toHaveBeenCalledWith(actions.fetchHealthySuccess({}));
+        });
+    });
+    
+    it('should dispatch fetchHealthyError', function() {
+        global.fetch = jest.fn().mockImplementation( url => {
+            return new Promise( (res, rej) => rej(new Error));
+        });
+        const dispatch = jest.fn();
+
+        return actions.fetchHealthyStuff('candy')(dispatch).then(() => {
+            // Check that we made a request to the correct URL
+            expect(fetch).toHaveBeenCalledWith('/api/healthy/candy');
+            // Make sure that we dispatched the correct sync action
+            expect(dispatch).toHaveBeenCalledWith(actions.fetchHealthyRequest('candy'));
+            expect(dispatch).toHaveBeenCalledWith(actions.fetchHealthyError(new Error));
+        });
+    });
+});
+
--- test/test-api.js
+++ test/test-api.js
@@ -2,6 +2,10 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
 
+const Categories = require('../server/models/categorySchema');
+const UnhealthyFoods = require('../server/models/unhealthyFoodSchema');
+const HealthyFoods = require('../server/models/healthyFoodSchema');
+
 const {TEST_DATABASE_URL} = require('../server/config'); 
 const {app, runServer, closeServer} = require('../server');
 
@@ -98,7 +102,7 @@ describe('Api', function() {
     describe('get /api/healthy/:unhealthyfood', function() {
         it('should return healthyfoods for its corresponding unhealthy food', function() {
             return chai.request(app)
-                .get('/api/healthy/energy-drinks')
+                .get('/api/healthy/sweet-candy')
                 .then(function(res) {
                     res.should.have.status(200);
                     res.should.be.json;
@@ -107,4 +111,84 @@ describe('Api', function() {
                 });
         });
     });
-})
+    
+    describe('post /api/:category', function() {
+        it('should add a category', function() {
+            const CATEGORY = 'test-bullshit';
+            return chai.request(app)
+                    .post(`/api/${CATEGORY}`)
+                    .then(function(res) {
+                        res.should.have.status(201);
+                        res.should.be.json;
+                        res.body.should.be.a('object');
+                        Object.keys(res.body).length.should.be.at.least(1);
+                        return Categories.find({name: CATEGORY });
+                    })
+                    .then(res => {
+                        res[0].name.should.equal(CATEGORY);
+                    });
+        });
+    });
+    
+    describe('post /api/unhealthy/:unhealthyfood', function() {
+        it('should add an unhealthyfood', function() {
+            const BADFOOD = 'test-bullshit';
+            return chai.request(app)
+                    .post(`/api/unhealthy/${BADFOOD}`)
+                    .then(function(res) {
+                        res.should.have.status(201);
+                        res.should.be.json;
+                        res.body.should.be.a('object');
+                        Object.keys(res.body).length.should.be.at.least(1);
+                        return UnhealthyFoods.find({
+                            // fix this later
+                            category: 'unknown',
+                            name: BADFOOD
+                        });
+                    })
+                    .then(res => {
+                        res[0].name.should.equal(BADFOOD);
+                    });
+        });
+    });
+    
+    describe('post /api/:category/:unhealthyfood/:healthyfood', function() {
+        it('should add a healthyfood', function() {
+            const GOODFOOD = 'test-bullshit';
+            return chai.request(app)
+                    .post(`/api/healthy/TEST/${GOODFOOD}`)
+                    .then(function(res) {
+                        res.should.have.status(201);
+                        res.should.be.json;
+                        res.body.should.be.a('object');
+                        Object.keys(res.body).length.should.be.at.least(1);
+                        return HealthyFoods.find({
+                            correspondingUnhealthyFood: 'TEST',
+                            name: GOODFOOD
+                        });
+                    })
+                    .then(res => {
+                        res[0].name.should.equal(GOODFOOD);
+                    });
+        });
+    });
+    
+    describe('delete /api/:category/:unhealthyfood/:healthyfood', function() {
+        it('should delete a healthy food', function() {
+            const GOODFOOD = 'test-bullshit';
+            return chai.request(app)
+                    .delete(`/api/healthy/TEST/${GOODFOOD}`)
+                    .then(function(res) {
+                        res.should.have.status(204);
+                        Object.keys(res.body).length.should.equal(0);
+                        return HealthyFoods.find({
+                            correspondingUnhealthyFood: 'TEST',
+                            name: GOODFOOD
+                        });
+                    })
+                    .then(res => {
+                        res.length.should.equal(0);
+                    });
+        });
+    });
+});
